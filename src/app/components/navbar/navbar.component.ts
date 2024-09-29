4eb65c08ca0762b30d6a280cb6ae7dc81c764a7a
Gian Cabigting <giancabigting1026@gmail.com>
2024-09-30 00:09:34 +0800

    DEBUG: Fixing Git Issues

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,64 +1,74 @@
 import { Component, OnInit } from '@angular/core';
-import { MenubarModule } from 'primeng/menubar';
 import { MenuItem } from 'primeng/api';
-
+import { MenubarModule } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { RippleModule } from 'primeng/ripple';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MenubarModule],
+  imports: [
+    MenubarModule,
+    BadgeModule,
+    AvatarModule,
+    InputTextModule,
+    RippleModule,
+    CommonModule,
+  ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent implements OnInit {
   items: MenuItem[] | undefined;
 
   ngOnInit() {
-      this.items = [
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+      },
+      {
+        label: 'Features',
+        icon: 'pi pi-star',
+      },
+      {
+        label: 'Projects',
+        icon: 'pi pi-search',
+        items: [
           {
-              label: 'Home',
-              icon: 'pi pi-home'
+            label: 'Components',
+            icon: 'pi pi-bolt',
           },
           {
-              label: 'Features',
-              icon: 'pi pi-star'
+            label: 'Blocks',
+            icon: 'pi pi-server',
           },
           {
-              label: 'Projects',
-              icon: 'pi pi-search',
-              items: [
-                  {
-                      label: 'Components',
-                      icon: 'pi pi-bolt'
-                  },
-                  {
-                      label: 'Blocks',
-                      icon: 'pi pi-server'
-                  },
-                  {
-                      label: 'UI Kit',
-                      icon: 'pi pi-pencil'
-                  },
-                  {
-                      label: 'Templates',
-                      icon: 'pi pi-palette',
-                      items: [
-                          {
-                              label: 'Apollo',
-                              icon: 'pi pi-palette'
-                          },
-                          {
-                              label: 'Ultima',
-                              icon: 'pi pi-palette'
-                          }
-                      ]
-                  }
-              ]
+            label: 'UI Kit',
+            icon: 'pi pi-pencil',
           },
           {
-              label: 'Contact',
-              icon: 'pi pi-envelope'
-          }
-      ]
+            label: 'Templates',
+            icon: 'pi pi-palette',
+            items: [
+              {
+                label: 'Apollo',
+                icon: 'pi pi-palette',
+              },
+              {
+                label: 'Ultima',
+                icon: 'pi pi-palette',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Contact',
+        icon: 'pi pi-envelope',
+      },
+    ];
   }
 }
-
