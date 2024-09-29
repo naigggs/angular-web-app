f0dee0b6cce692e86265bd1811c4b4798d86e8bb
Naigggs <gdcabigting.dev@gmail.com>
2024-09-29 20:10:45 +0800

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, ButtonModule, NavbarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'WebApp';
+}
